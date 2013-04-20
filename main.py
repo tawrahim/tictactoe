7dbcd7ed7f5aa948f344e1829f54352119f60184
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-04-19 20:14:15 -0400

    Figured out how to use jinja

---
+++ main.py
@@ -0,0 +1,19 @@
+import webapp2
+import os
+import jinja2
+from google.appengine.api import users
+
+jinja_environment = jinja2.Environment(autoescape=True,
+        loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+                'greet': "It works! Hello from localhost"
+        }
+
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+        debug=True)
